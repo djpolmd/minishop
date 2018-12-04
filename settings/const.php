d55569d845ccb8a96e04c7a47d7d397a4014be13
root <djpolmd@gmail.com>
2018-12-04 18:16:00 +0200

    dsds

--- settings/const.php
+++ settings/const.php
@@ -3,7 +3,7 @@
 	const CURRENCY = "Lei";
 	const IPATH    = "img/unknown.jpg";
 	const IMG_UNKNW= "unknown.jpg";
-	const DName = "https://academy-shop-djpolmd.c9users.io/";
+	const DName = "http://89.149.81.39/DEMO/minishop/";
 	
 	$imgs_path = "img";
 	setlocale(LC_MONETARY, 'eu_DE');
